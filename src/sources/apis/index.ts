a7707bc4cd49dbcc4b75c6fc31b9706094b42375
Atif Siddiqui <atifsiddiqui35@gmail.com>
2026-07-04 10:33:57 +0530

    feat: register 4 new Tier 2 API providers

--- src/sources/apis/index.ts
+++ src/sources/apis/index.ts
@@ -1,7 +1,18 @@
 import type { ApiProvider } from "./base.js";
+import { adzuna } from "./adzuna.js";
+import { himalayas } from "./himalayas.js";
 import { remoteok } from "./remoteok.js";
+import { remotejobsorg } from "./remotejobsorg.js";
 import { remotive } from "./remotive.js";
+import { themuse } from "./themuse.js";
 
 export type { ApiProvider } from "./base.js";
 
-export const apiProviders: ApiProvider[] = [remoteok, remotive];
+export const apiProviders: ApiProvider[] = [
+  remoteok,
+  remotive,
+  themuse,
+  remotejobsorg,
+  himalayas,
+  adzuna,
+];
